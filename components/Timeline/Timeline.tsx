65b437d0b17112748e950017b3662969e81a080c
Luis Gustavo Ribeiro <luisgustavo.ribeiro@outlook.com>
2021-12-26 12:12:18 -0300

    finished

--- components/Timeline/Timeline.tsx
+++ components/Timeline/Timeline.tsx
@@ -2,36 +2,64 @@ import styles from './Timeline.module.css';
 import { Tile } from '../TimelineTile/Tile';
 import React from 'react';
 
-const items = [ 'Concepção', 'Planejamento', 'Execução', 'Encerramento' ];
+let items = [
+	{
+        step: 1,
+		label: 'Concepção',
+		status: 'active'
+	},
+	{
+        step: 2,
+		label: 'Planejamento',
+		status: 'inactive'
+	},
+	{
+        step: 3,
+		label: 'Execução',
+		status: 'inactive'
+	},
+	{
+        step: 4,
+		label: 'Encerramento',
+		status: 'inactive'
+	}
+];
 
 export const Timeline = () => {
-
 	const [activeIndex, setActiveIndex] = React.useState<number>(0);
 
-    const handleActiveIndex = (index:number) => {
-        setActiveIndex(index);
-        console.log(activeIndex);
-    };
-    
+	const handleActiveIndex = (index: number) => {
+		setActiveIndex(index);
+        changeStatus(index);
+	};
+
+    const changeStatus = (activeIndex:number) => {
+        items.map(item => {
+            if(item.step < (activeIndex + 1)) return item.status = 'completed'
+            if(item.step === (activeIndex + 1))  return item.status = 'active'
+            if(item.step > (activeIndex + 1)) return item.status = 'inactive'
+        });
+
+        console.log(items);
+    }
+
 	const mappedItems = items.map((item, index) => {
-        
-		const step = index + 1;
 
-        const handlePosition = () => {
-            if (step === 1) return 'start'
-            if (step !== 1 && step !== items.length) return 'middle'
-            if (step === items.length) return 'end'
-        }
+		const handlePosition = () => {
+			if (item.step === 1) return 'start';
+			if (item.step !== 1 && item.step !== items.length) return 'middle';
+			if (item.step === items.length) return 'end';
+		};
 
-        return (
-            <Tile
-                key={index}
-                handleClick={() => handleActiveIndex(index)}
-                position={handlePosition()}
-                title={`${step}. ${item}`}
-                isActive={activeIndex === index ? true : false}
-            />
-        );
+		return (
+			<Tile
+				key={index}
+                status={item.status}
+				handleClick={() => handleActiveIndex(index)}
+				tilePosition={handlePosition()}
+				label={`${item.step}. ${item.label}`}
+			/>
+		);
 	});
 
 	return (
@@ -40,7 +68,7 @@ export const Timeline = () => {
 				<span style={{ fontWeight: 600 }}>Ciclo de Vida do Projeto</span>
 				<span>Atualizado em 08/12/20</span>
 			</div>
-			{mappedItems}
+			{ mappedItems }
 		</div>
 	);
 };
--- components/TimelineTile/Tile.tsx
+++ components/TimelineTile/Tile.tsx
@@ -1,27 +1,30 @@
-import React from 'react'
-import styles from './Tile.module.css'
+import React from 'react';
+import styles from './Tile.module.css';
+import { CheckmarkRegular } from '@fluentui/react-icons';
 
 interface ITile {
-    stage?: string;
-    step?: 'new' | 'started' | 'ongoing' | 'completed';
-    position?: 'start' | 'middle' | 'end';
-    title?: string;
-    hasStarted?: boolean;
-    isCompleted?:boolean;
-    hasMore?: boolean;
-    isActive?:boolean;
-    handleClick: () => void;
+	step?: string;
+	status?: 'inactive' | 'active' | 'completed' | string;
+	tilePosition?: 'start' | 'middle' | 'end';
+	label?: string;
+	isActive?: boolean;
+	handleClick: () => void;
 }
 
 export const Tile = (props: ITile) => {
+	const activeClass = `${styles.tile} ${styles[`${props.tilePosition}`]} ${styles[props.status ? props.status : 'inactive']} ${styles.active}`;
+	const inactiveClass = `${styles.tile} ${styles[`${props.tilePosition}`]} ${styles[props.status ? props.status : 'inactive']}`;
 
-    const activeClass = `${styles.tile} ${styles[`${props.position}`]} ${styles[props.step ? props.step : 'started']} ${styles.active}`
-    const inactiveClass = `${styles.tile} ${styles[`${props.position}`]} ${styles[props.step ? props.step : 'started']}`
-
-    return (
-        <div className={props.isActive ? activeClass : inactiveClass} onClick={props.handleClick}>
-            <span className={styles.stageIndicator}></span>
-            <span className={styles.tileTitle}>{props.title}</span>
-        </div>
-    )
-}
+	return (
+		<div className={props.isActive ? activeClass : inactiveClass} onClick={props.handleClick}>
+			{props.status === 'completed' ? (
+				<span className={styles.stageIndicator}>
+					<CheckmarkRegular fontSize={16} />
+				</span>
+			) : (
+				<span className={styles.stageIndicator}></span>
+			)}
+			<span className={styles.tileTitle}>{props.label}</span>
+		</div>
+	);
+};
